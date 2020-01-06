ac775c926b130e1226b12e16b9ae02bb581e1d20
haoran <jianghaoran@outlook.com>
2020-01-06 10:43:51 +0800

    [FEAT] 注释

--- src/js/arith-metic/bubble-sort.js
+++ src/js/arith-metic/bubble-sort.js
@@ -1,11 +1,27 @@
+/**
+ * 交换数据
+ * @param {array} arr 
+ * @param {number} from 
+ * @param {number} to 
+ */
 function swap(arr, from, to) {
   [arr[from], arr[to]] = [arr[to], arr[from]];
 }
 
+/**
+ * 设置以什么规则排序
+ * @param {number} prev 
+ * @param {number} next 
+ */
 function judgeFun(prev, next) {
   return next - prev < 0;
 }
 
+/**
+ * 冒泡排序
+ * @param {array} arr 
+ * @param {function} judge 
+ */
 function bubbleSort(arr, judge = judgeFun) {
   let resArr = [...arr];
   let length = resArr.length;
