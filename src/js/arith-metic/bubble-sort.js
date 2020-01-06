53e3ad24bdfbbc55b4d044c97e6d7a2d3586248c
haoran <jianghaoran@outlook.com>
2020-01-06 10:41:45 +0800

    [FEAT] 冒泡排序

--- src/js/arith-metic/bubble-sort.js
+++ src/js/arith-metic/bubble-sort.js
@@ -1,5 +1,22 @@
-function bubbleSort(arr) {
-  let resArr = arr;
+function swap(arr, from, to) {
+  [arr[from], arr[to]] = [arr[to], arr[from]];
+}
+
+function judgeFun(prev, next) {
+  return next - prev < 0;
+}
+
+function bubbleSort(arr, judge = judgeFun) {
+  let resArr = [...arr];
+  let length = resArr.length;
+
+  for (let i = 0; i < length; i += 1) {
+    for (let j = 0; j < length - 1 - i; j += 1) {
+      if (judge(resArr[j], resArr[j+1])) {
+        swap(resArr, j, j+1);
+      }
+    }
+  }
   return resArr;
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,6 @@ import { bubbleSort } from './arith-metic';
 
 const arr = [2,4,5,1,5,6,7,89,9,10] 
 const resArr = bubbleSort(arr);
+// const resArr = bubbleSort(arr, (prev, next) => prev - next > 0);
 console.log(arr);
 console.log(resArr);
