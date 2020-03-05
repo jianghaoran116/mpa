db32bb3de9309bc574f976bbbff43ac1410db53d
haoran <jianghaoran@outlook.com>
2020-03-05 18:09:10 +0800

    feat(functional): 函数式
    once & memoized

--- examples/functional/index.js
+++ examples/functional/index.js
@@ -1,9 +1,41 @@
 import wine from '../../src/main';
 
-const { tap } = wine;
+const {
+  tap,
+  forEach,
+  forEachObject,
+  once,
+  memoized,
+} = wine;
 
 [1, 2, 3].forEach((item) => {
   tap(item)(() => {
     console.log(item);
   })
 })
+
+forEach([1, 2, 3, 4], (item) => {
+  console.log(item);
+})
+
+forEachObject({a: 1}, (prototype, value) => {
+  console.log(prototype, value);
+})
+
+function click() {
+  console.log('click');
+}
+const onceClick = once(click);
+onceClick();
+onceClick();
+onceClick();
+
+function add(a) {
+  console.log('add');
+  return a + 1;
+}
+let memoizedAdd = memoized(add);
+let temp = memoizedAdd(1);
+console.log(temp);
+let temp2 = memoizedAdd(1);
+console.log(temp2);
--- src/functional/index.js
+++ src/functional/index.js
@@ -1,4 +1,46 @@
+/**
+ * 
+ * @param {any} value 
+ */
 export const tap = (value) => 
   (fn) => {
     typeof(fn) === 'function' && fn(value);
   }
+
+export const forEach = (array, fn) => {
+  for (let i = 0; i < array.length; i += 1) {
+    fn(array[i]);
+  }
+}
+
+export const forEachObject = (obj, fn) => {
+  for (let prototype in obj) {
+    if (Reflect.hasOwnProperty.call(obj, prototype)) {
+      fn(prototype, obj[prototype]);
+    }
+  }
+}
+
+/**
+ * 只能执行一次的函数
+ * @param {function} fn 
+ */
+export const once = (fn) => {
+  let done = false;
+
+  return function () {
+    return done
+      ? undefined
+      : ((done = true), fn.apply(this, arguments))
+  }
+}
+
+/**
+ * 缓存函数执行的结果
+ * @param {function} fn 
+ */
+export const memoized = (fn) => {
+  const lookupTable = {};
+  
+  return (args) => lookupTable[args] || (lookupTable[args] = fn(args))
+}
