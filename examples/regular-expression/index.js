745985281d4bec368fac3f3c23d38a1e84e5b70d
haoran <jianghaoran@outlook.com>
2020-05-06 16:06:15 +0800

    [FTR] 正则匹配全是数字和小数负数

--- examples/regular-expression/index.js
+++ examples/regular-expression/index.js
@@ -1,1 +1,22 @@
-console.log('regular rexpression')
+/**
+ * @file 正则表达式
+ * @author haoran
+ */
+
+// 只包含数字
+// ^[0-9]+$
+const regOnlyNum1 = /^[0-9]+$/;
+console.log(regOnlyNum1.test('123123adfadsf1123'));
+console.log(regOnlyNum1.test('1231231123'));
+console.log(regOnlyNum1.test('-123'));
+console.log(regOnlyNum1.test('123.12'));
+console.log(regOnlyNum1.test('-123.12'));
+
+// 允许出现负数和小数
+console.log('-------');
+const regOnlyNum2 = /^[-+]?[0-9]+(\.[0-9]*)?$/;
+console.log(regOnlyNum2.test('123123adfadsf1123'));
+console.log(regOnlyNum2.test('1231231123'));
+console.log(regOnlyNum2.test('-123'));
+console.log(regOnlyNum2.test('123.12'));
+console.log(regOnlyNum2.test('-123.12'));
